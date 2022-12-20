495eae3c9ddf5b35835b7ebacbc42dbeaae61261
Tatiana Ciocca <18190041+Tacial@users.noreply.github.com>
2022-12-20 17:59:39 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+     titulo: 'VEREADOR',
+     numeros: 5,
+     candidatos: [
+        {
+         numero: 38111,
+         nome: 'Fulano de Tal',
+         partido: 'ABC',
+         fotos: [
+            {url: '38111.jpg', legenda: 'Vereador', small: false}
+         ]
+        },
+        {
+         numero: 77222,
+         nome: 'Beltrana da Silva',
+         partido: 'DEFG',
+         fotos: [
+            {url: '77222.jpg', legenda: 'Vereador'}
+         ] 
+        },
+     ]
+    },
+    {
+     titulo: 'PREFEITO',
+     numeros: 2,
+     candidatos: [
+        {
+         numero: 74,
+         nome: 'Ciclano',
+         partido: 'ABC',
+         vice: 'Cic',
+         fotos: [
+            {url: '74.jpg', legenda: 'Prefeito'},
+            {url: '74_2.jpg', legenda: 'Vice-Prefeito', small: true}
+         ]        
+        },
+        {
+         numero: 84,
+         nome: 'Zuleano',
+         partido: 'DEFG',
+         vice: 'Zul',
+         fotos: [
+            {url: '84.jpg', legenda: 'Prefeito'},
+            {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+         ]        
+        },
+     ]
+    }
+];
